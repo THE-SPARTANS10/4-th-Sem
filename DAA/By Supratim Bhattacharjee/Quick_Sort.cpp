43cbc269a534e34c1c85b6ee20ec85284726eebf
THE-SPARTANS10 <79528033+THE-SPARTANS10@users.noreply.github.com>
2021-07-27 21:57:44 +0530

    Update Quick_Sort.cpp

--- DAA/By Supratim Bhattacharjee/Quick_Sort.cpp
+++ DAA/By Supratim Bhattacharjee/Quick_Sort.cpp
@@ -54,6 +54,14 @@ void quickSort(int* arr,int low,int high,int n)
 
 int main()
 {
+    /*
+    Input to be given :-
+
+    10
+    4 2 7 -678 83 122 1000 46 10 87
+
+    */
+
     fstream infile;
     infile.open("D:\\Engineering books and class notes\\sem4no\\Practical exam\\DAA\\input.txt", ios::in);
     if (!infile)
