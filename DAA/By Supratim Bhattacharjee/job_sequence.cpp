bbf55ae32aadc89c6beda5be8effca78ea52dbe1
THE-SPARTANS10 <79528033+THE-SPARTANS10@users.noreply.github.com>
2021-07-27 18:06:05 +0530

    Update job_sequence.cpp

--- DAA/By Supratim Bhattacharjee/job_sequence.cpp
+++ DAA/By Supratim Bhattacharjee/job_sequence.cpp
@@ -1,5 +1,6 @@
 //Name:- Supratim Bhattacharjee
 #include<iostream>
+#include<fstream>
 using namespace std;
 typedef long long ll;
 
@@ -7,7 +8,7 @@ typedef long long ll;
 Job sequencing with deadline(Greedy algorithm)
 ----------------------------
 Time complexity:-O(n^2)
-Space complexity:-O(n)
+Space complexity:-O(n) for extra array
 */
 
 class Job
@@ -28,20 +29,29 @@ class Job
 
 int main()
 {
+    fstream infile;
+    infile.open("D:\\Engineering books and class notes\\sem4no\\Practical exam\\DAA\\input.txt", ios::in);
+    if (!infile)
+    {
+        cout << "Error to open the file";
+        return 1;
+    }
+
     int n;
-    cout<<"Enter number of jobs: ";
-    cin>>n;
+    //cout<<"Enter number of jobs: ";
+    infile>>n;
     Job jobs[n];
     bool done[n]={0};
+    int jobsDoneSequence[n]={0};
     int numOfSlots=0;
     int maxProfit=0;
     for(ll i=0;i<n;i++)
     {
         int id,profit,deadline;
-        cout<<"Enter profit of job"<<i+1<<": ";
-        cin>>profit;
-        cout<<"Enter deadline of job"<<i+1<<": ";
-        cin>>deadline;
+        //cout<<"Enter profit of job"<<i+1<<": ";
+        infile>>profit;
+        //cout<<"Enter deadline of job"<<i+1<<": ";
+        infile>>deadline;
         Job currentJob=Job(i+1,profit,deadline);
         jobs[i]=currentJob;
     }
@@ -68,13 +78,21 @@ int main()
             {
                 numOfSlots++;
                 maxProfit+=jobs[i].profit;
+                jobsDoneSequence[j]=jobs[i].id;
                 done[j]=true;
                 break;
             }
         }
     }
 
-    cout<<"Maximum profit is: "<<maxProfit<<" and it will take "<<numOfSlots<<" slots"<<endl;
+
+    cout<<"Sequence of jobs done:- "<<endl;
+    for (int i=0;i<n;i++)
+       if(jobsDoneSequence[i])
+            cout<<"Job id. "<<jobsDoneSequence[i]<<"    ";
+    cout<<endl<<endl;
+
+    cout<<"Maximum profit is: "<<maxProfit<<"\nIt will take "<<numOfSlots<<" slots"<<endl;
 
     return 0;
 }
